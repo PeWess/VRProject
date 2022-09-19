f724b82cd4e5baac5ff78c0a5f6f09d0661bb8ad
Sergey <adv.sk2521@yandex.ru>
2022-09-19 15:27:38 +0300

    Completed homework 3

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    [SerializeField] public Transform turnPointTransform;
+    [Range(0, 1)] public double openPercent;
+
+    private Vector3 rotation_start = new Vector3(0, 0, 0);
+    private Vector3 rotation_end = new Vector3(0, -90, 0);
+
+    void Update()
+    {
+        openPercent = Vector3.Lerp(rotation_start, rotation_end, (turnPointTransform.rotation.eulerAngles.y - 360) / rotation_end.y).y / rotation_end.y;
+    }
+}
