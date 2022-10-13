e2b1846f2c61daa546cf655603e78987ebbcf011
Sergey <adv.sk2521@yandex.ru>
2022-10-13 11:15:05 +0300

    Completed homework 7

---
+++ Assets/Scripts/BreakInteraction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class BreakInteraction : MonoBehaviour
+{
+    private Interactable _interactableScript;
+    private Rigidbody _rigidBody;
+    
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+        _interactableScript = GetComponent<Interactable>();
+
+        _rigidBody.useGravity = false;
+        _rigidBody.isKinematic = true;
+        _interactableScript.enabled = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Cube"))
+        {
+            _rigidBody.useGravity = true;
+            _rigidBody.isKinematic = false;
+            _interactableScript.enabled = true;
+        }
+    }
+}
